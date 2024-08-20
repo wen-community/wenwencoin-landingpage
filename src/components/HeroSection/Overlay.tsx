26d467e37e598301acd4ef1de28acbebf1d6cc6d
Ali-Aizaz <50923380+Ali-Aizaz@users.noreply.github.com>
2024-08-20 23:13:26 +0500

    removed wen message

--- src/components/HeroSection/Overlay.tsx
+++ src/components/HeroSection/Overlay.tsx
@@ -1,14 +1,8 @@
 import Image from 'next/image'
 
-import { useState } from 'react'
-
-import { cn } from '@/utils/cn'
-
 import { PawPrint } from '../icons'
 
 const Overlay = () => {
-  const [isHovered, setIsHovered] = useState<boolean>(false)
-
   const handleScroll = () => {
     window.scrollTo({
       top: window.innerHeight * 1.8,
@@ -20,25 +14,11 @@ const Overlay = () => {
     <>
       <div className="relative w-max items-center">
         <Image
-          src="/message_box.png"
-          width={159}
-          height={156}
-          alt="Wen..."
-          className={cn(
-            'absolute top-10 z-10 w-28 opacity-0 transition-all ease-in-out md:w-40',
-            {
-              '-translate-x-20 opacity-100 md:-translate-x-36': isHovered
-            }
-          )}
-        />
-        <Image
           src="/wen_full_body.png"
           width={278.5}
           height={321}
-          className="relative z-20 w-40 md:h-80 md:w-72"
+          className="relative z-20 w-40 transition-transform duration-300 ease-in-out hover:scale-125 md:h-80 md:w-72"
           alt="Wen Wen Coin"
-          onMouseOver={() => setIsHovered(true)}
-          onMouseOut={() => setIsHovered(false)}
         />
       </div>
       <button
