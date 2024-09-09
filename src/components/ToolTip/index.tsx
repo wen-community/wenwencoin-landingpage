07cff2dcfc8a3643284f81375bc60d25daeead05
Ali-Aizaz <50923380+Ali-Aizaz@users.noreply.github.com>
2024-09-10 00:11:01 +0500

    Refactor ToolTip component to add interactive color swatches

---
+++ src/components/ToolTip/index.tsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react'
+
+import {
+  AnimatePresence,
+  motion,
+  useMotionValue,
+  useSpring,
+  useTransform
+} from 'framer-motion'
+
+import { cn } from '@/utils/cn'
+
+const ToolTip = ({
+  items
+}: {
+  items: {
+    id: number
+    name: string
+    hex: string
+  }[]
+}) => {
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null)
+  const springConfig = { stiffness: 100, damping: 5 }
+  const x = useMotionValue(0) // going to set this value on mouse move
+  // rotate the tooltip
+  const rotate = useSpring(
+    useTransform(x, [-100, 100], [-45, 45]),
+    springConfig
+  )
+  // translate the tooltip
+  const translateX = useSpring(
+    useTransform(x, [-100, 100], [-50, 50]),
+    springConfig
+  )
+  const handleMouseMove = (
+    event: React.MouseEvent<HTMLImageElement, MouseEvent>
+  ) => {
+    const halfWidth = event.currentTarget.offsetWidth / 2
+    x.set(event.nativeEvent.offsetX - halfWidth) // set the x value, which is then used in transform and rotate
+  }
+
+  return (
+    <>
+      {items.map((item) => (
+        <div
+          className="group relative space-x-4"
+          key={item.name}
+          onMouseEnter={() => setHoveredIndex(item.id)}
+          onMouseLeave={() => setHoveredIndex(null)}
+        >
+          <AnimatePresence mode="popLayout">
+            {hoveredIndex === item.id && (
+              <motion.div
+                initial={{ opacity: 0, y: 20, scale: 0.6 }}
+                animate={{
+                  opacity: 1,
+                  y: 0,
+                  scale: 1,
+                  transition: {
+                    type: 'spring',
+                    stiffness: 260,
+                    damping: 10
+                  }
+                }}
+                exit={{ opacity: 0, y: 20, scale: 0.6 }}
+                style={{
+                  translateX: translateX,
+                  rotate: rotate,
+                  whiteSpace: 'nowrap'
+                }}
+                className="absolute -left-1/2 -top-16 z-50 flex translate-x-1/2 flex-col items-center justify-center rounded-md bg-black px-4 py-2 text-xs shadow-xl"
+              >
+                <div className="absolute inset-x-10 -bottom-px z-30 h-px w-[20%] bg-gradient-to-r from-transparent via-emerald-500 to-transparent" />
+                <div className="absolute -bottom-px left-10 z-30 h-px w-[40%] bg-gradient-to-r from-transparent via-sky-500 to-transparent" />
+                <div className="relative z-30 text-base font-bold text-white">
+                  {item.name}
+                </div>
+                <div className="text-xs text-white">{item.hex}</div>
+              </motion.div>
+            )}
+          </AnimatePresence>
+          <div
+            onMouseMove={handleMouseMove}
+            className={cn(
+              'relative size-5 rounded-full border-2 !p-0 transition duration-500 group-hover:z-30 group-hover:scale-105',
+              `bg-[${item.hex}]`
+            )}
+          />
+        </div>
+      ))}
+    </>
+  )
+}
+
+export default ToolTip
