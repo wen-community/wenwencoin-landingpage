2260be62952b682fe2c4b11472a970c0e74a9cb6
Ali-Aizaz <50923380+Ali-Aizaz@users.noreply.github.com>
2024-09-10 23:20:12 +0500

    Refactor Pin component to update pin color
    Refactor the Pin component to update the fill color of the pin from "#DF5C4E" to "#4b5563".

--- src/pages/community.tsx
+++ src/pages/community.tsx
@@ -37,7 +37,7 @@ const JoinCommunity = () => {
             Add my location
           </button>
         </div>
-        <div className="flex flex-col items-end gap-2 rounded-lg bg-black/80 px-5 py-3 text-white">
+        <div className="flex flex-col items-end gap-2 rounded-lg bg-black/70 px-5 py-3 text-white">
           <h2 className="relative flex items-center gap-2 text-5xl font-semibold">
             <span className="relative top-0 flex h-3 w-3">
               <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
